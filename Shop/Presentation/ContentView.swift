77ee401ba19d7192c7572064c53804a6534ff345
Saeed Eskafi <saeed_es@felixin.io>
2026-02-16 09:07:03 +0330

    Initialize Shop app with main structure, assets, and test cases. Added ShopApp, ContentView, and asset configurations including AppIcon and AccentColor. Created unit and UI test files for Shop functionality.

---
+++ Shop/Presentation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shop
+//
+//  Created by Saeed on 2/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            Text("ShopApp")
+                .navigationTitle("Shop")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shop/ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopApp.swift
+//  Shop
+//
+//  Created by Saeed on 2/16/26.
+//
+
+import SwiftUI
+
+@main
+struct ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
